c5a839e8183918d24536066a19fd575d5388ccaa
tsilavina007 <andriamiart114@gmail.com>
2025-03-15 12:30:57 +0300

    🐞 fix: vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-	server: {
-		port: 8080,
-	},
+export default defineConfig(() => ({
 	plugins: [
 		react()
 	],
