c71295c704b0be9a7670760fa6a576c84db0d821
tsilavina007 <andriamiart114@gmail.com>
2025-03-15 12:41:40 +0300

    🐞 fix: env.PORT

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,18 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
+const portd = process.env.PORT || '5173';
+
 // https://vitejs.dev/config/
 export default defineConfig(() => ({
+	server:
+		{
+			port: Number(portd),
+		},
+
 	plugins: [
-		react()
+		[react()],
+
 	],
 	resolve: {
 		alias: {
