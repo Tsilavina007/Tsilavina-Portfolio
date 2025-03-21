9f4e30c7410e2a37131fd6a1791ed6ca83f968c5
tsilavina007 <andriamiart114@gmail.com>
2025-03-21 09:17:04 +0300

    🐞 fix: historyApiFallback on vite-config

--- vite.config.ts
+++ vite.config.ts
@@ -4,21 +4,21 @@ import path from "path";
 
 const portd = process.env.PORT || '5173';
 
-// https://vitejs.dev/config/
 export default defineConfig(() => ({
-	server:
-		{
-			port: Number(portd),
-			historyApiFallback: true,
-		},
-	plugins: [
-		[react()],
-
-	],
-	resolve: {
-		alias: {
-			"@": path.resolve(__dirname, "./src"),
-		},
-	},
+  server: {
+    port: Number(portd),
+  },
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, "index.html"),
+      },
+    },
+  },
 }));
-
